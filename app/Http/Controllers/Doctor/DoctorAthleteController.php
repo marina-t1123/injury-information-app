d6d57d0aee7499072e4cdac7dbb5698a61ae277c
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-19 22:03:37 +0900

    ドクター側で表示する選手詳細のController。

---
+++ app/Http/Controllers/Doctor/DoctorAthleteController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Doctor;
+
+use App\Http\Controllers\Controller;
+use App\Models\Athlete;
+
+class DoctorAthleteController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:doctors');
+    }
+
+    //選手詳細画面の表示
+    public function show($athlete_id)
+    {
+        $athlete = Athlete::findOrFail($athlete_id);
+
+        return view('athlete.show', compact('athlete'));
+    }
+}
