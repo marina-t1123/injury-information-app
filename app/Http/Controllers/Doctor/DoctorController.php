6e5422655f9176f26308593f15aff59748f37ba2
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-20 13:53:28 +0900

    コード共通化、条件分岐追加。

--- app/Http/Controllers/Doctor/DoctorController.php
+++ app/Http/Controllers/Doctor/DoctorController.php
@@ -15,6 +15,7 @@ class DoctorController extends Controller
      *
      * @param \Illuminate\Database\Eloquent\Collection||null $athletes
      * @param \Illuminate\Database\Eloquent\Collection||null $todayMedicalQuestionnaires
+     * @param Carbon\Carbon $today
      * @return void
      */
     public function doctorMyPage(Request $request)
@@ -23,25 +24,24 @@ class DoctorController extends Controller
         $athletes = Athlete::select('id', 'name', 'team', 'event', 'event_detail')->paginate(6);
 
         //受診日が今日の問診票を選手情報と一緒に取得
-        $todayMedicalQuestionnaires =
-            MedicalQuestionnaire::where('hospital_day', Carbon::today())
-                ->leftJoin('athletes', 'medical_questionnaires.athlete_id', '=', 'athletes.id')
-                ->with('medicalRecord')
-                ->paginate(1);
+        $todayMedicalQuestionnaires = MedicalQuestionnaire::getTodayMedicalQuestionnaires();
 
         //今日の日付を取得
         $today = Carbon::today()->format('Y年n月j日');
 
-        //選手検索フォームで検索された際の検索処理
-        //検索フォームでの入力された値を取得
-        $search = $request->search;
+        if(!empty($request))
+        {
+            //選手検索フォームで検索された際の検索処理
+            //検索フォームでの入力された値を取得
+            $search = $request->search;
 
-        //入力値をもとに登録済みの選手の中から、入力条件に合う選手を検索する
-        //Athleteモデルで登録されているローカルスコープ(選手の部分一致検索)を使用する。(プレフィックスのscopeはつけない)
-        $query = Athlete::searchAthlete($search);
+            //入力値をもとに登録済みの選手の中から、入力条件に合う選手を検索する
+            //Athleteモデルで登録されているローカルスコープ(選手の部分一致検索)を使用する。(プレフィックスのscopeはつけない)
+            $query = Athlete::searchAthlete($search);
 
-        //$queryにwhere句のクエリビルダーが入っているので、そこにselect句でマイページで表示する選手情報とページネーションを追加する。
-        $athletes = $query->select('id', 'name', 'team', 'event', 'event_detail')->paginate(6);
+            //$queryにwhere句のクエリビルダーが入っているので、そこにselect句でマイページで表示する選手情報とページネーションを追加する。
+            $athletes = $query->select('id', 'name', 'team', 'event', 'event_detail')->paginate(6);
+        }
 
         //マイページに遷移する(選手検索があった場合は検索結果を渡す)
         return view('doctor.mypage', compact('athletes', 'todayMedicalQuestionnaires', 'today'));
