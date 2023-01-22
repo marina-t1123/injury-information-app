b3ce51811841a1f09fef07af6851682d246ac3e2
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-22 22:58:53 +0900

    コードの共通化。

--- app/Http/Controllers/Doctor/DoctorMedicalHistoryController.php
+++ app/Http/Controllers/Doctor/DoctorMedicalHistoryController.php
@@ -20,9 +20,7 @@ class DoctorMedicalHistoryController extends Controller
         //既往歴を表示する選手を取得
         $athlete = Athlete::findOrFail($athlete_id);
         //選手に紐ずく既往歴を取得する
-        $medicalHistories = Athlete::findOrFail($athlete_id)
-            ->MedicalHistories()
-            ->paginate(4);
+        $medicalHistories = Athlete::getAthleteAndMedicalHistory($athlete_id)->paginate(4);
 
         //既往歴メニューページに遷移する
         return view('medical-history.medical-history-menu', compact('athlete', 'medicalHistories'));
@@ -32,7 +30,7 @@ class DoctorMedicalHistoryController extends Controller
     public function show($medical_history_id)
     {
         //詳細表示する既往歴を取得
-        $medicalHistory = MedicalHistory::find($medical_history_id)->with('athlete')->first();
+        $medicalHistory = MedicalHistory::getMedicalHistoryAndAthleteData($medical_history_id);
 
         //既往歴詳細ページで表示する既往歴を詳細ページへ渡して、リダイレクトする
         return view('medical-history.show', compact('medicalHistory'));
