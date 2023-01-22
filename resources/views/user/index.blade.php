15064fbf9b9f0def4567a8ea0fdff0f3dda3c527
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-23 00:55:43 +0900

    トレーナー一覧のblade作成。

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,89 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-700 leading-tight text-center">
+            トレーナー一覧
+        </h2>
+    </x-slot>
+
+    <!-- フラッシュメッセージ -->
+    <x-flash-message status="info" />
+
+    <div class="w-11/12 mx-auto mt-6">
+        <div class="mx-auto">
+            <p class="text-gray-500 text-center">現在登録されているトレーナーを表示しています。</p>
+        </div>
+
+        <!-- トレーナー検索 -->
+        <div class="w-1/2 h-70 bg-gray-300 mx-auto rounded-lg mt-9 flex justify-center shadow">
+            <x-user-search-form></x-user-search-form>
+        </div>
+
+        <!-- トレーナー一覧 -->
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+
+                <div class="flex flex-wrap -m-4">
+                    @foreach($users as $user)
+                        <div class="xl:w-1/2 md:w-1/2 w-11/12 mx-auto">
+                            <div class="bg-white border border-gray-200 p-6 m-2 rounded-lg min-w-500 shadow">
+
+                                <h3 class="text-lg text-gray-900 font-medium title-font mb-2">{{ $user->name }}</h3>
+                                <!-- トレーナーID -->
+                                <div class="flex justify-start">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+                                    </svg>
+                                    <p class="pl-1 leading-relaxed text-base">ID：{{ $user->id }}</p>
+                                </div>
+                                <!-- 名前 -->
+                                <div class="flex justify-start">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                                    </svg>
+                                    <p class="pl-1 leading-relaxed text-base">{{ $user->name }}</p>
+                                </div>
+                                <!-- Email -->
+                                <div class="flex justify-start">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                    </svg>
+                                    <p class="text-gray-500">{{ $user->email }}</p>
+                                </div>
+                                <!-- 電話番号 -->
+                                <div class="flex justify-start">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                                    </svg>
+                                    <p class="text-gray-500">{{ $user->userAttribute->phone_number }}</p>
+                                </div>
+                                <!-- 所属 -->
+                                <div class="flex justify-start">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                                    </svg>
+                                    <p class="text-gray-500">{{ $user->userAttribute->team }}</p>
+                                </div>
+                                <!-- 経歴 -->
+                                <div class="flex justify-start">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                    </svg>
+                                    <p class="text-gray-500">経歴<br>{{ $user->userAttribute->career }}</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    @endforeach
+                </div>
+
+                <!-- ページネーション -->
+                @if (!empty($users))
+                    <div class="mx-auto mt-10">{{ $users->links() }}</div>
+                @endif
+
+            </div>
+            <!-- マイページボタン -->
+            <x-mypage-button></x-mypage-button>
+        </section>
+    </div>
+</x-app-layout>
