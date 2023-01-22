560499137e0015a513757720a2a69503f0cb151d
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-22 23:37:27 +0900

    URLによって使用するCookieを使用するかの判定を行う処理を追加。

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     * 任意のアプリケーションサービスを登録します。
      *
      * @return void
      */
@@ -18,11 +19,18 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     * あらゆるアプリケーションサービスをブートストラップします。
      *
      * @return void
      */
     public function boot()
     {
-        //
+        //URLによってどのCookieを使うか判定をかける
+        // doctorから始まるURLで使用する場合
+        if(request()->is('doctor*')){ //もし、doctorから始まるURLの場合
+            config(['session.cookie' => config('session.cookie_doctor')]);
+            //doctorのcookieを使用する
+            //configのヘルパ関数を使用して、config>session.phpのcookie_doctor(key名)のvalue(doctorのcookie名)を指定している。
+        }
     }
 }
