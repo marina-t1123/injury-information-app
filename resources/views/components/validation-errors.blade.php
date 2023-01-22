f4f6d903c73064239f19900585c7c9b0c4c33bf0
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-23 00:37:44 +0900

    バリデーションエラーのblade作成。

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,10 +1,10 @@
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-300">
+        <div class="font-medium text-red-300 text-center">
             {{ __('There is an error in the form entry.')}}
         </div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-300">
+        <ul class="mt-3 list-disc list-inside text-sm text-red-300 text-center">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
