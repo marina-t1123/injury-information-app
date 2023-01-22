7d2123f8f78319d707c34bb3e0f980ce3f623b33
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-22 23:00:33 +0900

    コメントの修正。

--- app/Http/Controllers/User/DoctorIndexController.php
+++ app/Http/Controllers/User/DoctorIndexController.php
@@ -11,23 +11,23 @@ class DoctorIndexController extends Controller
     //ユーザー一覧表示
     public function index(Request $request)
     {
-        //登録済みの全トレーナーを詳細情報と一緒に取得する
+        //登録済みの全ドクターを詳細情報と一緒に取得する
         $doctors = Doctor::with('doctorAttribute')->paginate(2);
 
         if(!empty($request))
         {
-            //トレーナー検索フォームで検索された際の検索処理
+            //ドクター検索フォームで検索された際の検索処理
             //検索フォームでの入力された値を取得
             $search = $request->search;
-            //入力値をもとに登録済みトレーナーの中から、入力条件に合うトレーナーを検索する
-            //doctorモデルで設定されているローカルスコープ(トレーナーの部分一致検索)を使用する。(プレフィックスのscopeはつけない)
+            //入力値をもとに登録済みドクターの中から、入力条件に合うドクターを検索する
+            //doctorモデルで設定されているローカルスコープ(ドクターの部分一致検索)を使用する。(プレフィックスのscopeはつけない)
             $query = Doctor::searchDoctor($search);
 
-            //$queryにwhere句のクエリビルダーが入っているので、そこにselect句でマイページで表示するトレーナー情報とページネーションを追加する。
+            //$queryにwhere句のクエリビルダーが入っているので、そこにselect句でマイページで表示するドクター情報とページネーションを追加する。
             $doctors = $query->with('doctorAttribute')->select('id', 'name', 'email')->paginate(2);
         }
 
-        //トレーナー一覧表示画面にリダイレクトする
+        //ドクター一覧表示画面にリダイレクトする
         return view('doctor.index', compact('doctors'));
     }
 }
