eff656e2ded640b447d04dc4296a15b50e7c250e
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-23 00:14:42 +0900

    エラーメッセージのcss修正。

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,13 +2,13 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
+        <div class="font-medium text-red-600 text-center">
             {{ __('There is an error in the form entry.')}}
         </div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li class="text-center">{{ $error }}</li>
             @endforeach
         </ul>
     </div>
