10340bf13d7064f4397144716251b5f4e58176e9
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-22 23:57:20 +0900

    inputタグの修正。buttonタグのcssの修正。

--- resources/views/athlete/edit.blade.php
+++ resources/views/athlete/edit.blade.php
@@ -44,7 +44,7 @@
                             <label for="phone_number" class="leading-7 text-sm text-gray-600">
                                 電話番号
                             </label>
-                            <input type="string" id="phone_number" name="phone_number" value="{{ $athlete->phone_number }}" placeholder="00-0000-0000 or 000-0000-0000" class="w-full bg-white bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <input type="tel" id="phone_number" name="phone_number" value="{{ $athlete->phone_number }}" placeholder="00-0000-0000 or 000-0000-0000" class="w-full bg-white bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                         </div>
                         <!-- 所属 -->
@@ -86,7 +86,7 @@
                         </div>
                         <!-- ボタン -->
                         <div class="p-2 flex flex-col w-2/4 mt-8 mx-auto">
-                            <button type="submit" id="button" class="h-20 text-white bg-gray-600 border-0 py-2 px-8 focus:outline-none hover:bg-gray-500 rounded text-lg">
+                            <button type="submit" id="button" class="text-white bg-gray-600 border-0 py-2 px-8 focus:outline-none hover:bg-gray-500 rounded text-lg">
                                 送信
                             </button>
                             <button type="button" onclick="location.href='{{ route('user.mypage') }}'" class="text-white bg-gray-800 border-0 py-2 px-8 mt-5 focus:outline-none hover:bg-gray-700 rounded text-lg">
