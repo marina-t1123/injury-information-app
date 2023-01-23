038ac5d5faa1d4ea5302c393973bb0af43b250af
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-23 16:57:24 +0900

    トップページのblade作成。

---
+++ resources/views/top-page.blade.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
+        </style>
+        <!-- viteを使用せず、app.cssとapp.jsを使用する -->
+        <!-- styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js')}}" defer></script>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            <div class="fixed top-0 right-0 px-6 py-4 sm:block">
+                <div x-data="{ isOpen: true }" class="relative inline-block">
+                    <!-- Dropdown toggle button -->
+                    <button @click="isOpen = !isOpen" class="relative z-10 block p-2 text-gray-700 bg-white border border-transparent rounded-md dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:bg-gray-800 focus:outline-none">
+                        <svg class="w-5 h-5 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+
+                    <!-- Dropdown menu -->
+                    <div x-show="isOpen"
+                        @click.away="isOpen = false"
+                        x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="opacity-0 scale-90"
+                        x-transition:enter-end="opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-100"
+                        x-transition:leave-start="opacity-100 scale-100"
+                        x-transition:leave-end="opacity-0 scale-90"
+                        class="absolute right-0 z-20 w-48 py-2 mt-2 origin-top-right bg-white rounded-md shadow-xl dark:bg-gray-800"
+                    >
+                        @auth('users')
+                            <a href="{{ url('/mypage') }}" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> トレーナーマイページ </a>
+                        @endauth
+                        @auth('doctors')
+                            <a href="{{ url('/doctor/mypage')}}" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> ドクターマイページ </a>
+                        @endauth
+                        <a href="{{ route('user.register') }}" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> トレーナー登録</a>
+                        <a href="{{ route('user.login') }}" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> トレーナーログイン </a>
+                        <a href="{{ route('doctor.register') }}" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> ドクター登録 </a>
+                        <a href="{{ route('doctor.login') }}" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> ドクターログイン </a>
+                    </div>
+                </div>
+            </div>
+
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth('users')
+                        <a href="{{ url('/mypage') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">トレーナーマイページ</a>
+                    @endauth
+                    @auth('doctors')
+                        <a href="{{ url('/doctor/mypage') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ドクターマイページ</a>
+                    @endauth
+                </div>
+            {{-- @if (Route::has('user.login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth('users')
+                        <a href="{{ url('/mypage') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">トレーナーマイページ</a>
+                    @else
+                        <a href="{{ route('user.login') }}" class="text-xs text-gray-700 dark:text-gray-500 underline">トレーナー登録</a>
+
+                        @if (Route::has('user.register'))
+                            <a href="{{ route('user.register') }}" class="ml-2 text-xs text-gray-700 dark:text-gray-500 underline">トレーナーログイン</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif --}}
+
+
+            <div class="flex lg:w-1/2 w-full flex-col items-center justify-center mt-20 rounded-lg border border-gray-200 p-8">
+
+
+                <div class="mt-4 mb-4 text-center">
+                    <!-- アプリ名 -->
+                    <h1 class="lg:text-4xl md:text-3xl text-2xl text-white mb-8">Injury Information</h1>
+                    <!-- top画像-->
+                    <x-top-page-image></x-top-page-image>
+                    <!-- アプリ説明 -->
+                    <p class="mx-auto mt-4 lg:w-4/5 text-gray-500">
+                        Injury Information はトレーナーとドクターが選手の傷害情報を共有することで、選手のサポートを円滑に行うためのサービスです。
+                        また、受傷後の問診票とカルテを記録して共有することで傷害情報の管理をサポートします。
+                    </p>
+                </div>
+
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+                        <div class="sm:w-1/2 mb-10 px-4">
+                            <div class="rounded-lg h-64 overflow-hidden">
+                                <x-top-page-trainer-image></x-top-page-trainer-image>
+                            </div>
+                            <h2 class="title-font text-2xl font-medium text-gray-600 mt-6 mb-3">Athletic Trainer</h2>
+                            <p class="leading-relaxed text-base text-gray-500">
+                                選手のアカウント管理・既往歴・問診票の管理を行います。受傷後に問診票を作成し共有することでドクターとスムーズに選手情報を共有することができます。<br>
+                            </p>
+                            @auth('users')
+                                <a href="{{ url('/mypage') }}">
+                                    <button type="button" class="flex mx-auto mt-6 text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">マイページ</button>
+                                </a>
+                            @endauth
+                            <a href="{{ route('user.register') }}">
+                                <button class="flex mx-auto mt-6 text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">新規作成</button>
+                            </a>
+                            <a href="{{ route('user.login') }}">
+                                <button type="button" class="flex mx-auto mt-6 text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">ログイン</button>
+                            </a>
+                        </div>
+                        <div class="sm:w-1/2 mb-10 px-4">
+                            <div class="rounded-lg h-64 overflow-hidden">
+                                <x-top-page-doctor-image></x-top-page-doctor-image>
+                            </div>
+                            <h2 class="title-font text-2xl font-medium text-gray-600 mt-6 mb-3">Doctor</h2>
+                            <p class="leading-relaxed text-base text-gray-500">
+                                カルテの編集を行い、トレーナーとカルテ内容を共有することができます。選手のアカウント情報・既往歴・問診票などの情報も閲覧することができます。
+                            </p>
+                            @auth('doctors')
+                                <a href="{{ url('/doctor/mypage') }}">
+                                    <button type="button" class="flex mx-auto mt-6 text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">マイページ</button>
+                                </a>
+                            @endauth
+                            <a href="{{ route('doctor.register') }}">
+                                <button type="button" class="flex mx-auto mt-6 text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">新規作成</button>
+                            </a>
+                            <a href="{{ route('doctor.login') }}">
+                                <button type="button" class="flex mx-auto mt-6 text-white bg-green-500 border-0 py-2 px-5 focus:outline-none hover:bg-green-600 rounded">ログイン</button>
+                            </a>
+                        </div>
+                    </div>
+                  </div>
+
+                {{-- <div class="mt-25 max-w-lg sm:mx-auto md:max-w-none">
+                    <div class="flex"> --}}
+                    {{-- <div class="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16"> --}}
+
+                        {{-- <!-- トレーナーの新規登録・ログイン -->
+                        <div class="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
+                            <div class="flex h-12 w-12 items-center justify-center rounded-xl bg-green-500 text-white sm:shrink-0">
+                                <!-- Heroicon name: outline/globe-alt -->
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                            </div>
+                            <div class="sm:min-w-0 sm:flex-1">
+                                <p class="text-lg font-semibold leading-8 text-white">Trainer Register And Login</p>
+                                <p class="mt-2 text-base leading-7 text-gray-600">Register as a new trainer or log in.</p>
+                                <a href="route('user.register')" class="mt-3 text-green-500 inline-flex items-center">Trainer Register
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                                <a href="route('user.register')" class="mt-3 text-green-500 inline-flex items-center">Trainer Login
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- ドクターの新規作成・ログイン -->
+                        <div class="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
+                            <div class="flex h-12 w-12 items-center justify-center rounded-xl bg-green-500 text-white sm:shrink-0">
+                                <!-- Heroicon name: outline/scale -->
+                                <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0012 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.031.352 5.988 5.988 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 01-2.031.352 5.989 5.989 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971z" />
+                                </svg>
+                            </div>
+                            <div class="sm:min-w-0 sm:flex-1">
+                                <p class="text-lg font-semibold leading-8 text-gray-900">No hidden fees</p>
+                                <p class="mt-2 text-base leading-7 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div> --}}
+
+            </div>
+
+        </div>
+    </body>
+</html>
