0aafa7c4d6471353166a679ff1be6e6a3ad18043
marina-t1123 <75340313+marina-t1123@users.noreply.github.com>
2023-01-23 23:54:14 +0900

    未使用部分の削除。

--- resources/views/athlete/create.blade.php
+++ resources/views/athlete/create.blade.php
@@ -83,14 +83,6 @@
                             <textarea id="career" name="career" class="w-full bg-white bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ old('career') }}</textarea>
                             </div>
                         </div>
-                        {{-- <div class="p-2 w-3/4 mx-auto">
-                            <div class="relative">
-                            <label for="career" class="leading-7 text-sm text-gray-600">
-                                経歴
-                            </label>
-                            <input type="text" id="career" name="career" value="{{ old('career') }}" class="w-full bg-white bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                            </div>
-                        </div> --}}
                         <!-- ボタン -->
                         <div class="p-2 flex flex-col w-2/4 mt-8 mx-auto">
                             <button type="submit" id="button" class="text-white bg-gray-600 border-0 py-2 px-8 focus:outline-none hover:bg-gray-500 rounded text-lg">
